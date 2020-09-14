9836c74a80b469de0b0cf1b718d79d2d0c7f2d21
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 23:57:48 +0300

    ночные посиделки

---
+++ src/main/java/jolyjdia/test/Cities.java
@@ -0,0 +1,26 @@
+package jolyjdia.test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Cities {
+    private Map<Character, List<String>> map;
+    private char lastLetter;
+    private static final int SKIP = 1 << 'ь' | 1 << 'ъ' | 1 << 'ы';
+
+    private String randCity() {
+        List<String> cities = map.get(lastLetter);
+        String city = cities.get(ThreadLocalRandom.current().nextInt(cities.size()));
+        for(int i = city.length()-1; i > 0; --i) {
+            char c = city.charAt(i);
+            if(!Character.isLetter(c) || (SKIP >> c & 1) != 0) {
+                continue;
+            }
+            lastLetter = Character.toUpperCase(c);
+            cities.remove(city);
+            return city;
+        }
+        return randCity();
+    }
+}
